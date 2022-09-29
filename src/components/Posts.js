b8a35f1fc0f1f96685a73677118c2e3d7aeea651
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-29 15:02:53 +0300

    include nested routes

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import {Link, Outlet} from "react-router-dom"
 export default function Posts() {
   return (
-    <div>Posts</div>
+    <div>
+        <h1>Posts</h1>
+        <div className='sub-nav'>
+            <Link to ="post-list">Posts</Link>
+            <Link to = "addpost">Add Post</Link>
+        </div>
+        <Outlet/>
+    </div>
   )
 }
