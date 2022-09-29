066c1d8c76a9e367d1e2dca01e338a13c3d67a47
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-29 21:36:29 +0300

    render data from PostData

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,51 +1,31 @@
 import React from 'react'
-
+import { PostsData } from '../PostsData'
+import { useNavigate } from 'react-router-dom'
 export default function PostList() {
-  return (
-    <>
-      <div class="posts">
-        <div className='title'>
-          <h2>Available Posts</h2>
-        </div>
-        <div class="search">
-            <input type="text" class="searchTerm" placeholder="What post are you looking for?"/>
-            <button type="submit" class="searchButton">
-              <i class="fa fa-search"></i>
-           </button>
-        </div>
-        <div class="post">
-            <div class="post-item">
-                <div class="desc">
-                    <p>Description-1-1</p>
-                </div>
-                <img src="https://via.placeholder.com/150" alt="post"/>
+    const navigate = useNavigate()
+    return (
+        <>
+        <div class="posts">
+            <div className='title'>
+            <h2>Available Posts</h2>
             </div>
-        </div>
-        <div class="post">
-            <div class="post-item">
-                <div class="desc">
-                    <p>Description-1-1</p>
-                </div>
-                <img src="https://via.placeholder.com/150" alt="post"/>
+            <div class="search">
+                <input type="text" class="searchTerm" placeholder="What post are you looking for?"/>
+                <button type="submit" class="searchButton">
+                <i class="fa fa-search"></i>
+            </button>
             </div>
-        </div>
-        <div class="post">
-            <div class="post-item">
-                <div class="desc">
-                    <p>Description-1-1</p>
+            {PostsData.map((post) => 
+                <div class="post">
+                    <div class="post-item" key={post.id}>
+                        <div class="desc"> 
+                            <h4 onClick={() => {navigate(`/posts/${post.title}`);}}>{post.title}</h4>
+                        </div>
+                        <img src={post.image} alt="post"/>
+                    </div>
                 </div>
-                <img src="https://via.placeholder.com/150" alt="post"/>
-            </div>
+            )}
         </div>
-        <div class="post">
-            <div class="post-item">
-                <div class="desc">
-                    <p>Description-1-1</p>
-                </div>
-                <img src="https://via.placeholder.com/150" alt="post"/>
-            </div>
-        </div>
-      </div>
-    </>
-  )
+        </>
+    )
 }
