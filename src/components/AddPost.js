58239e9f04138c82bb6e38ef3097f79f599edd03
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-29 23:38:01 +0300

    add input form

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -2,6 +2,26 @@ import React from 'react'
 
 export default function AddPost() {
   return (
-    <div>AddPost</div>
+    <div>
+      <div className='posts addpost'>
+          <form>
+            <h2>Create Post</h2>
+            <div className='input-control'>
+              <label htmlFor='title'>Title</label>
+              <input type="text" placeholder="title"/>
+            </div>
+            <div className='input-control'> 
+              <label htmlFor='description'>Description</label>
+              <input type="textarea" placeholder='add description' />
+            </div>
+            <div className='input-control'>
+              <input type="file"/>
+            </div>
+            <div className='input-control'>
+              <button type='submit'>Add Post</button>
+            </div>
+          </form>
+      </div>
+    </div>
   )
 }
