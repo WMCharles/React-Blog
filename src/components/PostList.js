7cb04749912b3cc049056b6cc9bb02dcda49775e
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-29 16:23:13 +0300

    add Post Items

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,8 +2,44 @@ import React from 'react'
 
 export default function PostList() {
   return (
-    <div>
-        <h1>Available Posts</h1>
-    </div>
+    <>
+      <div class="posts">
+        <div className='title'>
+          <h2>Available Posts</h2>
+        </div>
+        <div class="post">
+            <div class="post-item">
+                <div class="desc">
+                    <p>Description-1-1</p>
+                </div>
+                <img src="https://via.placeholder.com/150" alt="post"/>
+            </div>
+        </div>
+        <div class="post">
+            <div class="post-item">
+                <div class="desc">
+                    <p>Description-1-1</p>
+                </div>
+                <img src="https://via.placeholder.com/150" alt="post"/>
+            </div>
+        </div>
+        <div class="post">
+            <div class="post-item">
+                <div class="desc">
+                    <p>Description-1-1</p>
+                </div>
+                <img src="https://via.placeholder.com/150" alt="post"/>
+            </div>
+        </div>
+        <div class="post">
+            <div class="post-item">
+                <div class="desc">
+                    <p>Description-1-1</p>
+                </div>
+                <img src="https://via.placeholder.com/150" alt="post"/>
+            </div>
+        </div>
+      </div>
+    </>
   )
 }
