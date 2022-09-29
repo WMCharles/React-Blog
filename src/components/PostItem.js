1489691cb4ce05b7f4d8a2230c473fc405fcc9e5
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-29 21:38:09 +0300

    add PostItem component

---
+++ src/components/PostItem.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { PostsData } from '../PostsData'
+
+export default function PostItem() {
+    const {title} = useParams()
+
+    return (
+        <div>
+            {PostsData.filter((post) => post.title === title).map((card) => 
+                <div className='postitem'>
+                    <h1>{card.title}</h1>
+                    <div className='shinobi'>
+                        <img src={card.image} alt="postimage"/>
+                    </div>
+                    <p>{card.description}</p>
+                </div>
+            )}
+        </div>
+    )
+}
