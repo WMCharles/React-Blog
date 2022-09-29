93e52af067605f349325c71745f779c8563e0707
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-29 15:09:45 +0300

    add Data

---
+++ PostsData.js
@@ -0,0 +1,20 @@
+export const ProductsData = [
+    {
+      id: 1,
+      title: "Post 1",
+      description: "Description 1",
+      image: "https://via.placeholder.com/150",
+    },
+    {
+      id: 2,
+      title: "Post 2",
+      description: "Description 2",
+      image: "https://via.placeholder.com/150",
+    },
+    {
+      id: 3,
+      name: "Post 3",
+      description: "Description 3",
+      image: "https://via.placeholder.com/150",
+    },
+  ];
