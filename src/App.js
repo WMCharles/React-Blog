107b5aa40cf34f398630d80aae27d2de138b4cdb
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-29 14:49:07 +0300

    add Routes & NavBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import About from './components/About';
+import AddPost from './components/AddPost';
+import Home from './components/Home';
+import Posts from './components/Posts';
+import NoPage from './components/NoPage';
+import NavBar from './components/NavBar';
+
 import './App.css';
 
 function App() {
   return (
-    <p>Hello</p>
+    <Router>
+      <Routes>
+        <Route path="/" element={<NavBar/>}>
+          <Route index element={<Home/>}/>
+          <Route path="about" element={<About/>}/>
+          <Route path="posts" element={<Posts/>}>
+            <Route path="addpost" element={<AddPost/>}/>
+          </Route>
+          <Route path="*" element={<NoPage/>}/>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
