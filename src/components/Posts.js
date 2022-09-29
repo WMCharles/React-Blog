954210b42a3042cbf2f2f3493f4ae810fa096fb0
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-29 16:23:36 +0300

    remove title

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,7 +3,6 @@ import {Link, Outlet} from "react-router-dom"
 export default function Posts() {
   return (
     <div>
-        <h1>Posts</h1>
         <div className='sub-nav'>
             <Link to ="post-list">Posts</Link>
             <Link to = "addpost">Add Post</Link>
