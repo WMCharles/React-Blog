f0290a6bba0e08c29edb21213e01e6f5a47f3f22
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-29 23:37:00 +0300

    restructure Posts component

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,29 +1,13 @@
 import React from 'react'
-import { PostsData } from '../PostsData';
-import { useNavigate } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 export default function Posts() {
-  const navigate = useNavigate()
   return (
     <div>
-        <div class="posts">
-            <div class="search">
-                <input type="text" class="searchTerm" placeholder="What post are you looking for?"/>
-                <button type="submit" class="searchButton">
-                <i class="fa fa-search"></i>
-            </button>
-            </div>
-            {PostsData.map((post) => 
-                <div class="post">
-                    <div class="post-item" key={post.id}>
-                        <div class="desc"> 
-                            <h4 onClick={() => {navigate(`/posts/${post.title}`);}}>{post.title}</h4>
-                        </div>
-                        <img src={post.image} alt="post"/>
-                    </div>
-                </div>
-            )}
+        <div className='sub-nav'>
+          <Link to="addpost">Add Post</Link>
         </div>
+        <Outlet/>
     </div>
   )
 }
