043c10fc62cd8be7b2c775077548d20a0e6402f8
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-30 16:15:50 +0300

    add content to About component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='about'>
+      <h2>About This Project</h2>
+      <p>This entire project is aimed at exploring different concepts in React Js. Some of the concepts that have been touched include:</p>
+      <li>React-Router-DOM, including react router dom</li>
+      <li>React Fetch API</li>
+      <li>React Hooks: useState, useEffect, useParams, and useNavigate</li>
+      <li>React Lists & Keys</li>
+      <li>React Controlled Components</li>
+      <li>React Filtering</li>
+      <p>I hope this helps you grasp the above concepts like it did with me. Feel free to fork and spin it your way.</p>
+    </div>
   )
 }
