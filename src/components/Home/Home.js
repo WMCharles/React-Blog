d2fc860fbddf609afa18bc1e467829584b201b50
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-30 13:11:49 +0300

    delete PostData, add prop, and change class to className

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,21 @@
 import React from 'react'
-import { PostsData } from '../../data/PostsData';
 import { useNavigate } from 'react-router-dom';
 
-export default function Home() {
+export default function Home({PostsData}) {
   const navigate = useNavigate()
   return (
     <div>
-        <div class="posts">
-            <div class="search">
-                <input type="text" class="searchTerm" placeholder="What post are you looking for?"/>
-                <button type="submit" class="searchButton">
-                <i class="fa fa-search"></i>
+        <div className="posts">
+            <div className="search">
+                <input type="text" className="searchTerm" placeholder="What post are you looking for?"/>
+                <button type="submit" className="searchButton">
+                <i className="fa fa-search"></i>
             </button>
             </div>
             {PostsData.map((post) => 
-                <div class="post">
-                    <div class="post-item" key={post.id}>
-                        <div class="desc"> 
+                <div className="post" key={post.id}>
+                    <div className="post-item">
+                        <div className="desc"> 
                             <h4 onClick={() => {navigate(`/posts/${post.title}`);}}>{post.title}</h4>
                         </div>
                         <img src={post.image} alt="post"/>
