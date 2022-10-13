bf09886b491c4145c7ccb98d41eb11c46719d4f8
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-13 23:23:07 +0300

    add div

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,11 +5,13 @@ import Form from './Form'
 export default function Contact() {
     return (
       <div className='about'>
-          <h2>Contact Us</h2>
-          <p>
-            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
-            The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters
-          </p>
+          <div className='intro'>
+            <h2>Contact Us</h2>
+            <p>
+              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+              The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters
+            </p>
+          </div>
           <div className='contact'>
             <Address/>
             <Form/>
