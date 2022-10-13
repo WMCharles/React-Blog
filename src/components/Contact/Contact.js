9b424f133da984a38778f132bed09e17b9486818
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-13 22:19:49 +0300

    import children component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Address from './Address'
+import Form from './Form'
 
 export default function Contact() {
     return (
@@ -8,58 +10,9 @@ export default function Contact() {
             It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
             The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters
           </p>
-          <div>
-            <div className="address">
-
-              <div className='pobox'>
-                <div>
-                  <i class="fa-solid fa-location-dot"></i>
-                </div>
-                <div>
-                  <h4>Address</h4>
-                  <p>P.O Box 364, Kitale, Sabwani Road </p>
-                </div>
-              </div>
-
-              <div>
-                <div>
-                  <i class="fa-solid fa-phone"></i>
-                </div>
-                <div>
-                  <h4>Phone</h4>
-                  <p>+254 795 289 737</p>
-                </div>
-              </div>
-
-              <div>
-                <div>
-                  <i class="fa-solid fa-envelope"></i>
-                </div>
-                <div>
-                  <h4>Email</h4>
-                  <p>wafulacharles47@gmail.com</p>
-                </div>
-              </div>
-
-            </div>
-
-            <div>
-              <form>
-                <h2>Send Message</h2>
-                <div className='input-group'>
-                  <label>Name</label>
-                  <input type="text" />
-                </div>
-                <div className='input-group'>
-                  <label>Email</label>
-                  <input type="text" />
-                </div>
-                <div className='input-group'>
-                  <label>Type your message...</label>
-                  <textarea rows="5"></textarea>
-                </div>
-              </form>
-            </div>
+          <div className='contact'>
+            <Address/>
+            <Form/>
           </div>
       </div>
     )
